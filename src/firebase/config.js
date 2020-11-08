442affebedf9cd99035a6c830d5d2891eef7cdc3
GreggX <gregoryglez11@gmail.com>
2020-11-08 16:12:48 -0600

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_9Zy3ik1c4L2_Ac8NGkhbPuIxouu5nUc",
+  authDomain: "avoapi.firebaseapp.com",
+  databaseURL: "https://avoapi.firebaseio.com",
+  projectId: "avoapi",
+  storageBucket: "avoapi.appspot.com",
+  messagingSenderId: "666445236597",
+  appId: "1:666445236597:web:bbbd4584edf740acdb6b3c",
+  measurementId: "G-LS2WB5MPZR"
+};
